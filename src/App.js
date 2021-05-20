94dbb621b864994fa08d0231a786eb091f0fe4da
Ostrovskaya Elena <ka-lenka@inbox.ru>
2021-05-20 22:48:32 +0300

    task-3

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '@components/Header';
+import Layout from '@components/Layout';
+import Chat from "./pages/Chat";
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Layout>
+                <Chat />
+            </Layout>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Chat from "./pages/Chat";
+import App from './App';
+
 
 ReactDOM.render(
-  <Chat />,
+  <App />,
   document.getElementById("root"),
 )
 
