0123569155f9b1dcf089b9107906604722bb7a54
Ostrovskaya Elena <ka-lenka@inbox.ru>
2021-05-26 22:44:24 +0300

    add create-react-app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '@components/Header';
+import Header from './components/Header';
 import Layout from '@components/Layout';
 import Chat from "./pages/Chat";
 
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div css={{ textAlign: 'center' }}>
+            <h1>Home</h1>
+            
+        </div>
+    )
+}
+
+export default Home;
